e0c5c5dbd38f739030e235ddef82f2fa3726a3ea
Andreas Backx <andreas@backx.org>
2022-04-24 17:07:13 +0100

    Fixed bug where ADDRESS in toggle-saved wasn't formatted.

--- src/client/service.rs
+++ src/client/service.rs
@@ -3,12 +3,14 @@ use anyhow::Result;
 use crate::server::grpc::api::{
     spotifatius_client::SpotifatiusClient, ToggleSavedRequest,
 };
+use crate::shared::consts::ADDRESS;
 
 pub struct Service {}
 
 impl Service {
     pub async fn toggle_saved() -> Result<()> {
-        let mut client = SpotifatiusClient::connect("http://{ADDRESS}").await?;
+        let mut client =
+            SpotifatiusClient::connect(format!("http://{ADDRESS}")).await?;
         let request = tonic::Request::new(ToggleSavedRequest {});
         let response = client.toggle_saved(request).await?;
         println!(
